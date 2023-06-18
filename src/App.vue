2f4ecd0fe3b7d27b7098219bcb74c62d6e73fa24
18704466567 <1870446657@qq.com>
2023-06-18 23:50:24 +0800

    add:frontend

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+ <router-view/>
 </template>
 
-<style lang="scss">
+<style lang="scss" setup>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import App from './App.vue'
+
+import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from './router'
+
+
+// 使用封装的登录验证vue组件
+
+
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
